fbb460accf47d3fda95207fde5f0a54aae7d1fac
PepeTheJoker <pepethejoker3301@gmail.com>
2024-04-28 07:43:15 -0700

    Modification 4

--- run_shadow.py
+++ run_shadow.py
@@ -11,9 +11,11 @@ try:
         print("=============================================================")
         print(f"Начало работы с полиэдром '{name}'")
         start_time = time()
-        Polyedr(f"data/{name}.geom").draw(tk)
+        poly = Polyedr(f"data/{name}.geom")
+        poly.draw(tk)
         delta_time = time() - start_time
         print(f"Изображение полиэдра '{name}' заняло {delta_time} сек.")
+        print(f"Характеристика: {poly.length}")
         input("Hit 'Return' to continue -> ")
 except (EOFError, KeyboardInterrupt):
     print("\nStop")
