0bbce660fd76ea171118e295de3e88ef99f272c2
PepeTheJoker <pepethejoker3301@gmail.com>
2024-04-28 09:09:48 -0700

    New test

--- tests/test_modified.py
+++ tests/test_modified.py
@@ -40,3 +40,42 @@ class TestPyramid(unittest.TestCase):
             40 * (4 + 4 * sqrt(2)) * 2,
             self.polyedr.length
         )
+
+
+class TestPrism(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        fake_file_content = """50.0    0   45   90
+6   5   18
+-2	-2	2
+-2	-2	-2
+2	-2	2
+-2	4	2
+-2	4	-2
+2	4	2
+3	1    2    3
+3	4    5    6
+4	1    2    5    4
+4	2    3    6    5
+4	3    1    4    6"""
+        fake_file_path = 'data/prism.geom'
+        with patch('shadow.polyedr.open'.format(__name__),
+                   new=mock_open(read_data=fake_file_content)) as _file:
+            self.polyedr = Polyedr(fake_file_path)
+            _file.assert_called_once_with(fake_file_path)
+
+    def test_num_vertexes(self):
+        self.assertEqual(len(self.polyedr.vertexes), 6)
+
+    def test_num_facets(self):
+        self.assertEqual(len(self.polyedr.facets), 5)
+
+    def test_num_edges(self):
+        self.assertEqual(len(self.polyedr.edges), 18)
+
+    def test_length(self):
+        self.assertAlmostEqual(
+            50 * (12 + 4 * sqrt(2)) * 2,
+            self.polyedr.length
+        )
